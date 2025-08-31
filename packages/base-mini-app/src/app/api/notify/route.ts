d4bd0eb7121bd1fff42a0ddfa358e330438f91f6
Azeez Mumeen <azeezmumeen6@gmail.com>
2025-08-31 16:04:57 +0100

    fix buid error

--- packages/base-mini-app/src/app/api/notify/route.ts
+++ packages/base-mini-app/src/app/api/notify/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const body = await request.json();
+    // const body = await request.json();
     // const { fid, notification } = body;
 
     // const result = await sendFrameNotification({
